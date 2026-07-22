fcc245040f4ec2f7204f0263997e424a7964bf76
Vickiiv <virzivictoria@gmail.com>
2026-07-22 11:06:37 +0200

    add Score model with userId reference

---
+++ src/models/score.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IScore extends Document {
+  userId: Types.ObjectId;
+  score: number;
+  date: Date;
+}
+
+const scoreSchema = new Schema<IScore>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Score = model<IScore>("Score", scoreSchema);
